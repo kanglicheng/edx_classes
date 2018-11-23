fb6a0e621199855b7125e41484357b9b58e24dd8
kanglicheng <11033065+kanglicheng@users.noreply.github.com>
2018-11-22 19:00:43 -0500

    problem 4 run simulation

--- pset2/ps2.py
+++ pset2/ps2.py
@@ -252,7 +252,6 @@ def runSimulation(num_robots, speed, width, height, min_coverage, num_trials,
     """
     Runs NUM_TRIALS trials of the simulation and returns the mean number of
     time-steps needed to clean the fraction MIN_COVERAGE of the room.
-
     The simulation is run with NUM_ROBOTS robots of type ROBOT_TYPE, each with
     speed SPEED, in a room of dimensions WIDTH x HEIGHT.
 
@@ -265,7 +264,23 @@ def runSimulation(num_robots, speed, width, height, min_coverage, num_trials,
     robot_type: class of robot to be instantiated (e.g. StandardRobot or
                 RandomWalkRobot)
     """
-    raise NotImplementedError
+    result = list()
+    for i in range(num_trials):
+        #anim = ps2_visualize.RobotVisualization(num_robots, width, height)
+        room = RectangularRoom(width,height)
+        robots = [robot_type(room,speed) for numRobots in range(num_robots)]
+        numSteps = 0
+        while (room.getNumCleanedTiles()/room.getNumTiles()) < min_coverage:
+            numSteps += 1
+            #anim.update(room, robots)
+            for robt in robots:
+                robt.updatePositionAndClean()
+            if (room.getNumCleanedTiles()/room.getNumTiles()) >= min_coverage:
+                result.append(numSteps)
+                #anim.done()
+            else:
+                continue
+    return sum(result)/len(result)
 
 # Uncomment this line to see how much your simulation takes on average
 ##print(runSimulation(1, 1.0, 10, 10, 0.75, 30, StandardRobot))
